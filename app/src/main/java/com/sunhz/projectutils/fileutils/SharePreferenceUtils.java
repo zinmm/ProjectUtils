f3e3e4825bd39925e6ae03d02d04aada22340310
ChinaSunHZ <china.sunhz@gmail.com>
2015-01-31 02:12:50 +0800

    add get* method

--- app/src/main/java/com/sunhz/projectutils/fileutils/SharePreferenceUtils.java
+++ app/src/main/java/com/sunhz/projectutils/fileutils/SharePreferenceUtils.java
@@ -1,47 +1,67 @@
 package com.sunhz.projectutils.fileutils;
 
-import java.util.Set;
-
 import android.content.Context;
 
 public class SharePreferenceUtils {
 
-	private static SharePreferenceUtils sharePreferenceUtils;
-	private Context mContext;
+    private static SharePreferenceUtils sharePreferenceUtils;
 
-	private SharePreferenceUtils() {
+    private Context mContext;
 
-	}
+    private SharePreferenceUtils(Context mContext) {
+        this.mContext = mContext;
+    }
 
-	private SharePreferenceUtils(Context mContext) {
-		this.mContext = mContext;
-	}
+    public static SharePreferenceUtils getInstance(Context mContext) {
+        if (sharePreferenceUtils != null) {
+            sharePreferenceUtils = new SharePreferenceUtils(mContext);
+        }
+        return sharePreferenceUtils;
+    }
 
-	public static SharePreferenceUtils getInstance(Context mContext) {
-		if (sharePreferenceUtils != null) {
-			sharePreferenceUtils = new SharePreferenceUtils(mContext);
-		}
-		return sharePreferenceUtils;
-	}
+    public void saveLong(String spName, String key, long content) {
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putLong(key, content)
+                .commit();
+    }
 
-	public void saveLong(String spName, String key, long content) {
-		mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putLong(key, content).commit();
-	}
+    public long getLong(String spName, String key) {
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).getLong(key, -1);
+    }
 
-	public void saveInt(String spName, String key, int content) {
-		mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putLong(key, content).commit();
-	}
+    public void saveInt(String spName, String key, int content) {
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putLong(key, content)
+                .commit();
+    }
 
-	public void saveBoolean(String spName, String key, boolean content) {
-		mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putBoolean(key, content).commit();
-	}
+    public int getInt(String spName, String key) {
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).getInt(key, -1);
+    }
 
-	public void saveString(String spName, String key, String content) {
-		mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putString(key, content).commit();
-	}
+    public void saveBoolean(String spName, String key, boolean content) {
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putBoolean(key, content)
+                .commit();
+    }
 
-	public void saveFloat(String spName, String key, float content) {
-		mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putFloat(key, content).commit();
-	}
+    public boolean getBoolean(String spName, String key) {
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).getBoolean(key, false);
+    }
+
+    public void saveString(String spName, String key, String content) {
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putString(key, content)
+                .commit();
+    }
+
+    public String getString(String spName, String key) {
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).getString(key, "");
+    }
+
+    public void saveFloat(String spName, String key, float content) {
+        mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).edit().putFloat(key, content)
+                .commit();
+    }
+
+    public float getFloat(String spName, String key) {
+        return mContext.getSharedPreferences(spName, Context.MODE_PRIVATE).getFloat(key, -1);
+    }
 
 }
