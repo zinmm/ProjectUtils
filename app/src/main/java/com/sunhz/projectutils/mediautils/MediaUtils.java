75934c269a0b5318c1a715f8f554d69b25300945
ChinaSunHZ <china.sunhz@gmail.com>
2015-01-31 02:13:36 +0800

    update getInstance

--- app/src/main/java/com/sunhz/projectutils/mediautils/MediaUtils.java
+++ app/src/main/java/com/sunhz/projectutils/mediautils/MediaUtils.java
@@ -7,32 +7,33 @@ import android.net.Uri;
 import java.io.File;
 
 /**
- * 多媒体相关工具类 Created by spencer on 2015/1/30.
+ * 多媒体相关工具类 Created by Spencer on 2015/1/30.
  */
 public class MediaUtils {
 
     private static MediaUtils mediaUtils;
 
-    private MediaUtils() {
+    private Context mContext;
 
+    private MediaUtils(Context mContext) {
+        this.mContext = mContext;
     }
 
-    public synchronized static MediaUtils getInstance() {
+    public synchronized static MediaUtils getInstance(Context mContext) {
         if (mediaUtils == null) {
-            mediaUtils = new MediaUtils();
+            mediaUtils = new MediaUtils(mContext);
         }
         return mediaUtils;
     }
 
     /**
      * 向媒体库中插入一条数据,进行媒体更新
-     * @param mContext
-     * @param file 待插入媒体文件在手机中的绝对路径
-     * @param mediaScanCompletedCallBack  插入完成后的回调
+     *
+     * @param file                       待插入媒体文件在手机中的绝对路径
+     * @param mediaScanCompletedCallBack 插入完成后的回调
      */
-    public void mediaScan(Context mContext, File file,
-            final MediaScanCompletedCallBack mediaScanCompletedCallBack) {
-        MediaScannerConnection.scanFile(mContext, new String[] { file.getAbsolutePath() }, null,
+    public void mediaScan(File file, final MediaScanCompletedCallBack mediaScanCompletedCallBack) {
+        MediaScannerConnection.scanFile(mContext, new String[]{file.getAbsolutePath()}, null,
                 new MediaScannerConnection.OnScanCompletedListener() {
 
                     @Override
