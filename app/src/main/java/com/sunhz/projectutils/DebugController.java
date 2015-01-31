e25acca430854721fdac7b71b06d3c317df1ba04
ChinaSunHZ <china.sunhz@gmail.com>
2015-01-31 17:19:00 +0800

    update debugFlag in BuildConfig.Debug

--- app/src/main/java/com/sunhz/projectutils/DebugController.java
+++ app/src/main/java/com/sunhz/projectutils/DebugController.java
@@ -2,6 +2,12 @@ package com.sunhz.projectutils;
 
 public class DebugController {
 
-	public static final boolean isDebug = false;
+    /**
+     * 现在使用BuildConfig来进行管理
+     * 若使用eclipse,请进行手动更改.
+     * 这个标志只能在android studio中使用
+     * 若想在eclipse中使用,请将eclipse的自动编译关闭
+     */
+	public static final boolean isDebug = BuildConfig.DEBUG;
 
 }
