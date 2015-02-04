679b748874ca82b5814181f83bc153c574c5289f
ChinaSunHZ <china.sunhz@gmail.com>
2015-02-04 10:42:18 +0800

    add PackageUtils

--- app/src/main/java/com/sunhz/projectutils/packageutils/PackageUtils.java
+++ app/src/main/java/com/sunhz/projectutils/packageutils/PackageUtils.java
@@ -1,7 +1,61 @@
 package com.sunhz.projectutils.packageutils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import java.io.File;
+
 /**
+ * 包管理工具
  * Created by Spencer on 15/2/3.
  */
 public class PackageUtils {
+
+    private static PackageUtils packageUtils;
+
+    private Context mContext;
+
+    private PackageUtils(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public static PackageUtils getInstance(Context mContext) {
+        if (packageUtils == null) {
+            packageUtils = new PackageUtils(mContext);
+        }
+        return packageUtils;
+    }
+
+    /**
+     * 安装本地apk
+     *
+     * @param apkFilePath 本地apk路径
+     */
+    public void install(String apkFilePath) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(new File(apkFilePath)),
+                "application/vnd.android.package-archive");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(intent);
+    }
+
+
+    /**
+     * 卸载本地apk
+     *
+     * @param packageName 本地apk包名
+     */
+    public void unInstall(String packageName) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_DELETE);
+        intent.setData(Uri.parse("package:" + packageName));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mContext.startActivity(intent);
+    }
+
+    public void getAllPackageName(){
+
+    }
+
 }
